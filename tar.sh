6edf26f431985fd8912d29469a5d06da14cca11c
KuzRomanDocker <49670235+KuzRomanDocker@users.noreply.github.com>
2019-04-16 10:54:24 +0300

    Update tar.sh

--- tar.sh
+++ tar.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-tar -czvf arch.tar.gz [0-9][0-9][0-9][0-9]* | while read I ; do rmdir "$I" ; done
+when=$(date '+%Y-%m-%d')
+
+tar -czvf $when.tar.gz [0-9][0-9][0-9][0-9]* | while read I ; do rm -rf "$I" ; done
